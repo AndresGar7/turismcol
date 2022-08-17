d1e5bf5bab19a6c33199292b11861fe204753543
Andres <eddy_gc07@hotmail.com>
2022-08-17 18:01:18 +0200

    Editando README

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -38,85 +38,86 @@
         </section>
     @else
         @if ($usuario->rol == 'user') 
-        <section class="content">
-            <div class="container-fluid">
-                <div class="card card-success shadow-lg">
-                    <div class="card-header">
-                        <h2 class="card-title fw-bold fs-3 mt-1">Panel Informativo</h2>
-                    </div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-6">
-                                <div class="card card-success card-outline">
-                                    <div class="card-body box-profile">
-                                        <div class="text-center">
-                                            <img class="profile-user-img img-fluid img-circle" src="{{ asset($masDatos[0]->url_img) }}" alt="User profile picture">
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="card card-success shadow-lg">
+                        <div class="card-header">
+                            <h2 class="card-title fw-bold fs-3 mt-1">Panel Informativo</h2>
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-6">
+                                    <div class="card card-success card-outline">
+                                        <div class="card-body box-profile">
+                                            <div class="text-center">
+                                                <img class="profile-user-img img-fluid img-circle" src="{{ asset($masDatos[0]->url_img) }}" alt="User profile picture">
+                                            </div>
+                                            <h3 class="profile-username text-center">{{ $masDatos[0]->nombre }} {{ $masDatos[0]->apellidos }}</h3>
+                                            <p class="text-muted text-center">{{ strtoupper($usuario->rol) }}</p>
+                                            <ul class="list-group list-group-unbordered mb-3">
+                                                <li class="list-group-item">
+                                                    <b>Usuario</b> <a class="float-right">{{ $masDatos[0]->email }}</a>
+                                                </li>
+                                                <li class="list-group-item">
+                                                    <b>Fec. Nacimiento</b> <a class="float-right">{{ $masDatos[0]->fec_nac }}</a>
+                                                </li>
+                                                <li class="list-group-item">
+                                                    <b>Telefono</b> <a class="float-right">{{ $masDatos[0]->telefono }}</a>
+                                                </li>
+                                            </ul>
+                                            <a href="{{ route('perfil.admin') }}" class="btn btn-primary btn-block"><b>Ver Perfil</b></a>
                                         </div>
-                                        <h3 class="profile-username text-center">{{ $masDatos[0]->nombre }} {{ $masDatos[0]->apellidos }}</h3>
-                                        <p class="text-muted text-center">{{ strtoupper($usuario->rol) }}</p>
-                                        <ul class="list-group list-group-unbordered mb-3">
-                                            <li class="list-group-item">
-                                                <b>Usuario</b> <a class="float-right">{{ $masDatos[0]->email }}</a>
-                                            </li>
-                                            <li class="list-group-item">
-                                                <b>Fec. Nacimiento</b> <a class="float-right">{{ $masDatos[0]->fec_nac }}</a>
-                                            </li>
-                                            <li class="list-group-item">
-                                                <b>Telefono</b> <a class="float-right">{{ $masDatos[0]->telefono }}</a>
-                                            </li>
-                                        </ul>
-                                        <a href="{{ route('perfil.admin') }}" class="btn btn-primary btn-block"><b>Ver Perfil</b></a>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-6">
-                                <div class="row">
-                                    <div class="col-lg-6 col-6">
-                                        <div class="small-box bg-info">
-                                            <div class="inner">
-                                                <h3>150</h3>
-                                                <p>New Orders</p>
+                                <div class="col-6">
+                                    <div class="row">
+                                        <div class="col-lg-6 col-6">
+                                            <div class="small-box bg-info">
+                                                <div class="inner">
+                                                    <h3>150</h3>
+                                                    <p>New Orders</p>
+                                                </div>
+                                                <div class="icon">
+                                                    <i class="ion ion-bag"></i>
+                                                </div>
+                                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                                             </div>
-                                            <div class="icon">
-                                                <i class="ion ion-bag"></i>
-                                            </div>
-                                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                                         </div>
-                                    </div>
-                                    <div class="col-lg-6 col-6">
-                                        <div class="small-box bg-success">
-                                            <div class="inner">
-                                                <h3>53<sup style="font-size: 20px">%</sup></h3>
-                                                <p>Bounce Rate</p>
+                                        <div class="col-lg-6 col-6">
+                                            <div class="small-box bg-success">
+                                                <div class="inner">
+                                                    <h3>53<sup style="font-size: 20px">%</sup></h3>
+                                                    <p>Bounce Rate</p>
+                                                </div>
+                                                <div class="icon">
+                                                    <i class="ion ion-stats-bars"></i>
+                                                </div>
+                                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                                             </div>
-                                            <div class="icon">
-                                                <i class="ion ion-stats-bars"></i>
-                                            </div>
-                                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                                         </div>
-                                    </div>
-                                    <div class="col-lg-6 col-6">
-                                        <div class="small-box bg-warning">
-                                            <div class="inner">
-                                                <h3>44</h3>
-                                                <p>User Registrations</p>
+                                        <div class="col-lg-6 col-6">
+                                            <div class="small-box bg-warning">
+                                                <div class="inner">
+                                                    <h3>44</h3>
+                                                    <p>User Registrations</p>
+                                                </div>
+                                                <div class="icon">
+                                                    <i class="ion ion-person-add"></i>
+                                                </div>
+                                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                                             </div>
-                                            <div class="icon">
-                                                <i class="ion ion-person-add"></i>
-                                            </div>
-                                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                                         </div>
-                                    </div>
-                                    <div class="col-lg-6 col-6">
-                                        <div class="small-box bg-danger">
-                                            <div class="inner">
-                                                <h3>65</h3>
-                                                <p>Unique Visitors</p>
+                                        <div class="col-lg-6 col-6">
+                                            <div class="small-box bg-danger">
+                                                <div class="inner">
+                                                    <h3>65</h3>
+                                                    <p>Unique Visitors</p>
+                                                </div>
+                                                <div class="icon">
+                                                    <i class="ion ion-pie-graph"></i>
+                                                </div>
+                                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                                             </div>
-                                            <div class="icon">
-                                                <i class="ion ion-pie-graph"></i>
-                                            </div>
-                                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                                         </div>
                                     </div>
                                 </div>
@@ -124,21 +125,97 @@
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
         @else
-        <section class="content">
-            <div class="container-fluid">
-                <div class="card card-success shadow-lg">
-                    <div class="card-header">
-                        <h2 class="card-title fw-bold fs-3 mt-1">Panel Informativo</h2>
-                    </div>
-                    <div class="card-body">
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="card card-success shadow-lg">
+                        <div class="card-header">
+                            <h2 class="card-title fw-bold fs-3 mt-1">Panel Informativo</h2>
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-lg-3 col-6">
+                                    <div class="small-box bg-info">
+                                        <div class="inner">
+                                            <h3>150</h3>
+                                            <p>New Orders</p>
+                                        </div>
+                                        <div class="icon">
+                                            <i class="fas fa-shopping-cart"></i>
+                                        </div>
+                                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-6">
+                                    <div class="small-box bg-success">
+                                        <div class="inner">
+                                            <h3>53<sup style="font-size: 20px">%</sup></h3>
+                                            <p>Bounce Rate</p>
+                                        </div>
+                                        <div class="icon">
+                                            <i class="ion ion-stats-bars"></i>
+                                        </div>
+                                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-6">
+                                    <div class="small-box bg-warning">
+                                        <div class="inner">
+                                            <h3>44</h3>
+                                            <p>User Registrations</p>
+                                        </div>
+                                        <div class="icon">
+                                            <i class="fas fa-user-plus"></i>
+                                        </div>
+                                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-6">
+                                    <div class="small-box bg-danger">
+                                        <div class="inner">
+                                            <h3>65</h3>
+                                            <p>Unique Visitors</p>
+                                        </div>
+                                        <div class="icon">
+                                            <i class="fas fa-chart-pie"></i>
+                                        </div>
+                                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-4">
+                                    <div class="card card-success card-outline">
+                                        <div class="card-body box-profile">
+                                            <div class="text-center">
+                                                <img class="profile-user-img img-fluid img-circle" src="{{ asset($masDatos[0]->url_img) }}" alt="User profile picture">
+                                            </div>
+                                            <h3 class="profile-username text-center">{{ $masDatos[0]->nombre }} {{ $masDatos[0]->apellidos }}</h3>
+                                            <p class="text-muted text-center">{{ strtoupper($usuario->rol) }}</p>
+                                            <ul class="list-group list-group-unbordered mb-3">
+                                                <li class="list-group-item">
+                                                    <b>Usuario</b> <a class="float-right">{{ $masDatos[0]->email }}</a>
+                                                </li>
+                                                <li class="list-group-item">
+                                                    <b>Fec. Nacimiento</b> <a class="float-right">{{ $masDatos[0]->fec_nac }}</a>
+                                                </li>
+                                                <li class="list-group-item">
+                                                    <b>Telefono</b> <a class="float-right">{{ $masDatos[0]->telefono }}</a>
+                                                </li>
+                                            </ul>
+                                            <a href="{{ route('perfil.admin') }}" class="btn btn-primary btn-block"><b>Ver Perfil</b></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-6">
 
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
         @endif
     @endif
 @stop
